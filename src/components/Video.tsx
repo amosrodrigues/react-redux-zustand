ae6f34e6e7b11df0ce2636d2ddfa4c73d4d0ae2e
Amós Rodrigues <amos.adm.rh@gmail.com>
2023-06-26 21:07:24 -0300

    feat: loading course data.

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -10,6 +10,10 @@ export function Video() {
     dispatch(next())
   }
 
+  if (!currentLesson) {
+    return null
+  }
+
   return (
     <div className="aspect-video w-full bg-zinc-950">
       <ReactPlayer
